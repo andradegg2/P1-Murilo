405d86f7b2bc95790e4e63675cfe2df534394e1b
goesa <goesa@DESKTOP-8KJ1AC1>
2023-05-01 12:38:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Carteira.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Carteira
+    {
+        public double Saldo
+        {
+            get;
+            private set;
+        }
+        public string Dono { get; set; }
+
+        public bool Sacar(double Valor)
+        {
+            if (Valor > this.Saldo)
+                return false;
+
+            this.Saldo -= Valor;
+            //this.Saldo = Saldo - Valor;
+            return true;
+        }
+
+        public bool Depositar(double Valor)
+        {
+            this.Saldo += Valor;
+            return true;
+        }
+
+        public bool Transferir
+            (Carteira destino, double valor)
+        {  
+            //se nao tiver saldo cancela transferencia retornando falso
+            if (this.Saldo <= valor)
+                return false;
+
+            //Executa transferencia tirando da conta principal e depositando na conta destino
+            this.Sacar(valor);
+            bool tOK = destino.Depositar(valor);
+            if (tOK)// se transferencia ocorreu com sucesso retorna true
+                return true;
+            else// caso ocorrer erro faz a reversão voltando dinheiro para conta principal
+            {
+                this.Depositar(valor);
+                return false;
+            }
+        }
+    }
+}
---
+++ Contas/LibClasses/Carteira.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas.LibClasses
+{
+    public class Carteira
+    {
+        public double Saldo
+        {
+            get;
+            private set;
+        }
+        public string Dono { get; set; }
+
+        
+        public double LimiteConta  // adiciona o limite na conta
+        {
+
+            get;
+            set;
+        }
+       
+        public DateTime Tarifa // adicionaa tarifa
+        {
+            get;
+            set;
+        }
+        
+        public bool cobrarTarifa(DateTime dataTarifa)
+        {
+            this.Saldo -= 19.90;
+            this.Tarifa = dataTarifa;
+            return true;
+        }
+        public string Cpf// adiciona cpf
+        {
+            get;
+            set;
+        }
+        
+        public bool Sacar(double Valor, DateTime dataSistema) //limita o horario para sacar
+        {
+
+            if (!(dataSistema.Hour >= 8 && dataSistema.Hour < 18))
+                return false;
+
+
+            if (Valor > (this.Saldo + LimiteConta ))
+                return false;
+
+            this.Saldo -= Valor;
+            //this.Saldo = Saldo - Valor;
+            return true;
+        }
+
+        public bool Sacar(double Valor)
+		{
+
+
+			if (Valor > this.Saldo )
+                return false;
+
+            this.Saldo -= Valor;
+            //this.Saldo = Saldo - Valor;
+            return true;
+        }
+
+        public bool Depositar(double Valor)
+        {
+            this.Saldo += Valor;
+            return true;
+        }
+
+        public bool Transferir
+            (Carteira destino, double valor)
+        {
+            //se nao tiver saldo cancela transferencia retornando falso
+            if (this.Saldo <= valor)
+                return false;
+
+            //Executa transferencia tirando da conta principal e depositando na conta destino
+            this.Sacar(valor);
+            bool tOK = destino.Depositar(valor);
+            if (tOK)// se transferencia ocorreu com sucesso retorna true
+                return true;
+            else// caso ocorrer erro faz a reversão voltando dinheiro para conta principal
+            {
+                this.Depositar(valor);
+                return false;
+            }
+        }
+    }
+}
